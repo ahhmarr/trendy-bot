37e255fe27732f7637320614b3fa131b440ece10
ahmar siddiqui <ahmar.siddiqui@gmail.com>
2016-07-26 09:46:25 +0530

    working alpha created

--- app/tweet.js
+++ app/tweet.js
@@ -5,7 +5,9 @@ var unirest=require('unirest');
 var twitterConfig=config.get('twitter');
 var twit=require('twit');
 var unirest=require('unirest');
-
+var tim=require('text-image-merger');
+var fs=require("fs");
+var T=new twit(twitterConfig);
 module.exports={
 	send : function()
 	{
@@ -14,18 +16,59 @@ module.exports={
 		.send()
 		.end(function(resp)
 		{
-			tweet(resp.body.word+':'+resp.body.meaning);
+			var txt=resp.body.word+':'+resp.body.meaning;
+			if(txt.length>0){
+				tweetImage(txt);
+			}else{
+				tweetText(txt);
+			}
 		});
 	}
-}
+};
 
-function tweet(msg){
+function tweetText(msg){
 	console.log('tweeting',msg);
-	var T=new twit(twitterConfig);
 	T.post('statuses/update',{
 		status : msg
 	},function(err,res,data)
 	{
-		
+		if(err){
+			console.log(err);
+		}
 	})
+}
+
+function tweetImage(msg)
+{
+	tim.generateTextImage(msg)
+	.then(function(imageURI)
+	{
+		// console.log(imageURI);
+		fs.writeFile('/var/tmp/img.txt',imageURI);
+		uploadImage(imageURI);
+	}).catch(function(err)
+	{
+		throw new Error(err);
+	});
+}
+
+function uploadImage(imageURI){
+	T.post('media/upload',{
+		media_data : imageURI.substr(22)
+	},function(err,data,resp)
+	{
+		if(err){
+			console.log(err);
+			return;
+		}
+		T.post('statuses/update',{
+			media_ids : [data.media_id_string]
+		},function(err,data,resp)
+		{
+			if(err){
+				console.log(err);
+			}
+		})
+		console.log(data.media_id_string);
+	});
 }
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var tweet=require('../app/tweet.js');
-
+var fs=require('fs');
 
 /* GET home page. */
 router.get('/tweet', function(req, res, next) {
   tweet.send();
   res.send('called me');
+});
+router.get('/64', function(req, res, next) {
+	var b64=fs.readFileSync('/home/ahmar/Pictures/3.png',{encoding:'base64'});
+	console.log(b64);
+  res.send(b64);
 });
 
 module.exports = router;
